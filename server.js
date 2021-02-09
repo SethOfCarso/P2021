31ec7b6f13ec4cc7715449e441d582da4fdb4129
SethOfCarso <cardona.erick@hotmail.com>
2021-02-08 21:21:57 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+/**
+ * Modulo principal para Primavera 2020
+ */
+const express = require('express');
+const cors = require('cors');
+const port = 3300;
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Back end para Primavera 2020')
+})
+
+app.listen(port, () => console.log("http://localhost:" + port));
