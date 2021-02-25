fa0c9d3d8b69553501ce579106b694661470341c
SethOfCarso <cardona.erick@hotmail.com>
2021-02-25 12:31:12 -0600

    added basic routes for arboles

--- routes/arboles.routes.js
+++ routes/arboles.routes.js
@@ -6,15 +6,25 @@ const arbolesController = require('../controllers/arboles.controllers');
 router.route('/tiposArboles')
     .get(arbolesController.getAllTaxonomiaArboles)
 
-router.route('/holaArbol')
-    .get(arbolesController.holaArbol)
+router.route('/imagenes')
+    .get(arbolesController.getAllImagenesArboles)
 
-router.route('/hey')
-    .get(
-        (req, res) =>{
-            res.send("hola")
-        }
-    )
+router.route('/inspeccion')
+    .get(arbolesController.getAllInspeccionArboles)
+
+router.route('/inventario')
+    .get(arbolesController.getAllInventarioArboles)
+
+router.route('/jardines')
+    .get(arbolesController.getAllJardinesArboles)
+
+router.route('/getAllNodosArboles')
+    .get(arbolesController.getAllNodosArboles)
+
+router.route('/rutas')
+    .get(arbolesController.getAllRutaArboles)
+
+
 
 
 module.exports = router;
