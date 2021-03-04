1513b7e04e238de9f550f46f7bbd9da47d88c556
SethOfCarso <cardona.erick@hotmail.com>
2021-03-04 16:01:11 -0600

    added documentation in JSDOC

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,15 +3,15 @@
 const router = require('express').Router();
 const userController = require('../controllers/user.controller');
 
-router.route('/test')
-    .get(userController.testUser)
+router.route('/login')
+    .post(userController.usuarioLogin)
 
-router.route('/hey')
-    .get(
-        (req, res) =>{
-            res.send("hola")
-        }
-    )
+
+router.route('/allLogins')
+    .get(userController.getAllUsers)
+
+router.route('/singleUser/:id')
+    .get(userController.singleUserByID)
 
 
 module.exports = router;
